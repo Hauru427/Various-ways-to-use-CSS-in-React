c021b5178179d618da37dcbd174ef923d4221d8a
Hauru427 <hauru.051427@gmail.com>
2024-09-28 16:03:03 +0900

    how to use CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { CssModules } from './components/CssModules';
 import { InlineStyles } from './components/InlineStyle';
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyles />
+      <CssModules />
     </div>
   );
 }
