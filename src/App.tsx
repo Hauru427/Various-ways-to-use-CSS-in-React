9ae32e657ae94e79021dc4ae876b0195b59778fb
Hauru427 <hauru.051427@gmail.com>
2024-09-28 16:30:56 +0900

    how to use styled JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { CssModules } from './components/CssModules';
 import { InlineStyles } from './components/InlineStyle';
+import { StyledJsx } from './components/StyledJsx';
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyles />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
